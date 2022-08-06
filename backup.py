62ca97b4ac49c66301f62d711feee0f6044f4b55
Artur Oliveira Silva Alves <arturoliveira88@outlook.com>
2022-08-06 19:18:01 -0300

    Cópia do arquivo nos dois dispositivos sendo o do Pen-drive mais antigo

--- backup.py
+++ backup.py
@@ -1,41 +1,21 @@
 def leArquivo(path):
     
     A = open(path, 'r')
-    lista_A = []
+    lista = []
     
     for i in A:
         i = i.rstrip()
-        lista_A.append(i)
-    '''    
-    temBackup = False
-    fazBackup = False
-    arqEHD = False
-    arqEPenDrive = False
-    dataPen<HD = False
-    dataPen==HD = False
-    dataPen>HD = False
-    '''    
+        lista.append(i)
         
-    if lista_A[0][-1] == 'F': # Verifica se há Backup.parm. 
+    # Verifica se há Backup.parm -----------------------------------------
+    if lista[0][-1] == 'F': 
         return 'Impossível'
     
-    if lista_A[1][-1] == 'V' and lista_A[2][-1] == 'V' and lista_A[3][-1] == 'F':
+    # Copia o arquivo do presente no HD para o Pen-drive -----------------
+    if lista[1][-1] == 'V' and lista[2][-1] == 'V' and lista[3][-1] == 'F':
         return 'Copia de HD para Pen-drive'
-    '''
-    if lista_A[2][-1] == 'V':
-        arqEHD = True
-        
-    if lista_A[3][-1] == 'V':
-        arqEPenDrive = True
     
-    if lista_A[4][-1] == 'V':
-        dataPen<HD = True
-    
-    if lista_A[5][-1] == 'V':
-        dataPen==HD = True
-    
-    if lista_A[6][-1] == 'V':
-        dataPen>HD = True
-        
-    '''
-leArquivo('./Testes/T2.txt')
+    # Copia o arquivo do presente no HD para o Pen-drive, considerando que
+    # o arquivo já presente no Pen-drive é mais antigo (data menor)  
+    if lista[1][-1] == 'V' and lista[2][-1] == 'V' and lista[3][-1] == 'V' and lista[4][-1] == 'V':
+        return 'Copia de HD para Pen-drive'
--- test_backup.py
+++ test_backup.py
@@ -1,11 +1,20 @@
 from backup import leArquivo
 
+# Verifica se há Backup.parm -----------------------------------------
 def test_1():
     entrada = leArquivo('./Testes/T1.txt')
     saida = 'Impossível'
     assert entrada == saida
-    
+
+# Copia o arquivo do presente no HD para o Pen-drive -----------------
 def test_2():
     entrada = leArquivo('./Testes/T2.txt')
+    saida = 'Copia de HD para Pen-drive'
+    assert entrada == saida
+
+# Copia o arquivo do presente no HD para o Pen-drive, considerando que
+# o arquivo já presente no Pen-drive é mais antigo (data menor) 
+def test_3():
+    entrada = leArquivo('./Testes/T3.txt')
     saida = 'Copia de HD para Pen-drive'
     assert entrada == saida
